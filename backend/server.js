3e666c0fe3014446c7c7ecf043727f8b20a9f868
ravipandeydu <rp120896@gmail.com>
2022-11-06 19:41:40 +0530

    Server Updated

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res)=>{
     res.send("Home Page")
 })
 
+app.get("/about", (req, res)=>{
+    res.send("About page")
+})
+
 app.listen(8070, ()=>{
     console.log("listening to Port 8070")
 })
